92b8faf2d418be18a712e2ae3c5a62d69ea63f61
Elad <eladkoren1@gmail.com>
2017-12-11 08:28:50 +0200

    add home activity and track activity

--- app/src/main/java/greenstory/rtg/com/HomeActivity.java
+++ app/src/main/java/greenstory/rtg/com/HomeActivity.java
@@ -1,21 +1,35 @@
 package greenstory.rtg.com;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+
 
 public class HomeActivity extends AppCompatActivity {
+    Intent trackIntent;
+    Button map1Button;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+
+        map1Button = (Button) findViewById(R.id.map1);
+        map1Button.setOnClickListener(buttonListener);
+        trackIntent = new Intent(this,TrackActivity.class);
     }
+
+    View.OnClickListener buttonListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            trackIntent.putExtra("MapNumber", 1);
+            startActivity(trackIntent);
+        }
+    };
+
 }
 
 

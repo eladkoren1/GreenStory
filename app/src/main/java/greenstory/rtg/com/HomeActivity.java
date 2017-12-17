b53161b0502821a52cfb93facde54cec50b4c971
Elad <eladkoren1@gmail.com>
2017-12-17 10:37:06 +0200

    upgraded home activity, added user activity, added helper class

--- app/src/main/java/greenstory/rtg/com/HomeActivity.java
+++ app/src/main/java/greenstory/rtg/com/HomeActivity.java
@@ -1,15 +1,45 @@
 package greenstory.rtg.com;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.view.ViewGroup.LayoutParams;
+import android.view.ViewGroup.MarginLayoutParams;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 
 public class HomeActivity extends AppCompatActivity {
-    Intent trackIntent;
-    Button map1Button;
+
+
+    ScrollView scrollView;
+    LinearLayout scrollChildLayout;
+    TextView trackTV;
+    Button addText;
+    public Context context = this;
+    public int textViewId = 1;
+    ArrayList<String> tracks = new ArrayList<>();
+    public int tracksListSize = 1;
 
 
     @Override
@@ -17,18 +47,82 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        map1Button = (Button) findViewById(R.id.map1);
-        map1Button.setOnClickListener(buttonListener);
-        trackIntent = new Intent(this,TrackActivity.class);
+        scrollView = (ScrollView) findViewById(R.id.scroller);
+        scrollChildLayout = (LinearLayout) findViewById(R.id.scroll_contents);
+
+
+        addText = (Button) findViewById(R.id.add_item_button);
+
+
+        addText.setOnClickListener(listener);
+
+
     }
 
-    View.OnClickListener buttonListener = new View.OnClickListener() {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.user_preferences_menu_item) {
+            Intent intent = new Intent(this, UserActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
+    View.OnClickListener listener = new View.OnClickListener() {
+        @TargetApi(Build.VERSION_CODES.O)
         @Override
-        public void onClick(View view) {
-            trackIntent.putExtra("MapNumber", 1);
-            startActivity(trackIntent);
+        public void onClick(View v) {
+
+
+            if (v.getId() == addText.getId()) {
+                scrollChildLayout.addView(getTextView());
+                Log.d("Pressed", "onClick: ");
+            }
+
+            if (v.getId() == trackTV.getId()) {
+                TelephonyManager manager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+                if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                String uId = manager.getImei();
+                Toast.makeText(context,uId,Toast.LENGTH_SHORT).show();
+
+                Log.d("Pressed", "onClick: ");
+            }
         }
     };
+
+    public TextView getTextView(){
+
+        trackTV = new TextView(context);
+        LayoutParams layoutParams = new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+
+        trackTV.setLayoutParams(layoutParams);
+        trackTV.setText("הר ארבל");
+        trackTV.setTextSize(30);
+        trackTV.setId(textViewId);
+        trackTV.setOnClickListener(listener);
+        textViewId++;
+
+        return trackTV;
+    }
+
 
 }
 
---
+++ app/src/main/java/greenstory/rtg/com/helpers/User.java
@@ -0,0 +1,11 @@
+package greenstory.rtg.com.helpers;
+
+/**
+ * Created by Elad on 17/12/2017.
+ */
+
+public class User {
+
+    private String u
+
+}

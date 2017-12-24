658f8024b0bfc2ce6e5eac826da8510a214f246b
elad - laptop <eladkoren1@gmail.com>
2017-12-24 18:10:01 +0200

    add maps

--- app/src/main/java/greenstory/rtg/com/MapsActivity.java
+++ app/src/main/java/greenstory/rtg/com/MapsActivity.java
@@ -3,6 +3,7 @@ package greenstory.rtg.com;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
@@ -14,6 +15,7 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.maps.android.data.kml.KmlLayer;
+import com.google.maps.android.data.kml.KmlPlacemark;
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -23,6 +25,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     private GoogleMap mMap;
     KmlLayer layer;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -49,9 +52,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
         try {
-            layer = new KmlLayer(googleMap, R.raw.campus_bkp_tests, getApplicationContext());
+            layer = new KmlLayer(googleMap, R.raw.Academic_TLV, getApplicationContext());
             layer.addLayerToMap();
-
         } catch (XmlPullParserException e) {
             e.printStackTrace();
         } catch (IOException e) {
@@ -68,6 +70,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             return;
         }
         mMap.setMyLocationEnabled(true);
+        try{
+            if (layer.getPlacemarks().iterator().hasNext()) {
+                KmlPlacemark placemark = layer.getPlacemarks().iterator().next();
+                LatLng markerLatLng = placemark.getMarkerOptions().getPosition();
+                LatLng cuurentLatLng = new LatLng();
+        }
+        }
+
 
     }
 }

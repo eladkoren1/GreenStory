72662d127ea017ae4f4164d5e2e31cdf989233dd
Elad <eladkoren1@gmail.com>
2017-12-26 23:27:26 +0200

    building user activity

--- app/src/main/java/greenstory/rtg/com/UserActivity.java
+++ app/src/main/java/greenstory/rtg/com/UserActivity.java
@@ -1,11 +1,21 @@
 package greenstory.rtg.com;
 
+import android.provider.Telephony;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 public class UserActivity extends AppCompatActivity {
 
+    private Button points;
+    private TextView mName;
+    private TextView mFamilyName;
+    private TextView mPartnerName;
+    private TextView mAge;
+    private TextView mPartnerAge;
+    private CheckBox mIsFamily;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +23,6 @@ public class UserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_user);
     }
 
-    private Button points;
+
 
 }

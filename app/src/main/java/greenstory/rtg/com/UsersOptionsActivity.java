268cfa6c8c2719662801e9480593d49ac0560c65
shays <shay.shwartz@gmail.com>
2018-01-03 11:16:01 +0200

    add activity of "UsersOptionsActivity"

--- app/src/main/java/greenstory/rtg/com/UsersOptionsActivity.java
+++ app/src/main/java/greenstory/rtg/com/UsersOptionsActivity.java
@@ -1,14 +1,18 @@
 package greenstory.rtg.com;
 
+import android.app.ActionBar;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 public class UsersOptionsActivity extends AppCompatActivity {
 
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
+    String[] mobileArray = {"ניקוד","כמות שאלות",
+            "אתרים","מיקום בטבלה","לחנות הוירטואלית"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +25,21 @@ public class UsersOptionsActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list_Of_Options_To_Users);
         listView.setAdapter(adapter);
 
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        getMenuInflater().inflate(R.menu.menu_users_options, menu);
+
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.edit_menu_item) {
+            //Intent intent = new Intent(this, UserActivity.class);
+            //startActivity(intent);
+        }
+        return true;
     }
 
 
